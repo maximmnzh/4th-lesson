b4495bfd305733e2bd47f2a18db7ff081b9e5125
Maxim Manzhos <maxim.mnzh@gmail.com>
2019-08-05 14:07:17 +0300

    A program that calculates the sum of numbers inside a string

--- IntegerExperiments/src/Main.java
+++ IntegerExperiments/src/Main.java
@@ -4,12 +4,27 @@ public class Main
     {
         Container container = new Container();
         container.count += 7843;
-
+        System.out.println(sumDigits(container.count));
     }
 
-    public Integer sumDigits(Integer number)
-    {
-        //@TODO: write code here
-        return 0;
+    static Integer sumDigits(Integer number) {
+        String s = number.toString();
+        int sum = 0;
+        for(int i = 0; i < s.length() ; i++){
+            if( Character.isDigit(s.charAt(i)) ){
+                sum = sum + Character.getNumericValue(s.charAt(i));
+            }
+        }
+        return sum;
+
+//        Without using toString and charAt methods.
+//        int sum = 0;
+//        int rest = 1;
+//        while(rest!=0){
+//            rest = number % 10;
+//            number = number / 10;
+//            sum = sum + rest;
+//        }
+//        return sum;
     }
 }
